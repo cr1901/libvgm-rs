c0c9702a6052f7c71f41b02b77fc94f3e43519c8
William D. Jones <thor0505@comcast.net>
2019-03-31 22:31:16 -0400

    Add bindings to libvgm-sys using bindgen.

---
+++ libvgm-sys/wrapper.h
@@ -0,0 +1,6 @@
+#include "emu/EmuStructs.h"
+#include "emu/SoundEmu.h"
+#include "emu/Resampler.h"
+#include "emu/SoundDevs.h"
+#include "emu/EmuCores.h"
+#include "emu/cores/sn764intf.h"	// for SN76496_CFG
